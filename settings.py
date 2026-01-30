c7d37b6891cd0c44b861f5a3c20f1c4fba17bb7f
dangeno <dgeneau@csipacific.ca>
2026-01-30 11:04:23 -0800

    update for deployment

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ AUTH_URL = f"{SITE_URL}/o/authorize"
 TOKEN_URL = f"{SITE_URL}/o/token/"
 CLIENT_ID = 'bDf3z9KwxSzCFtxabQ10UwlnHCMl2IsE5teZWLu4' #os.environ.get("CLIENT_ID")
 CLIENT_SECRET = 'em7L8NeqjKP8vxTEYRz7LrnHKz7aU8pm7t0DfbCiyQkljgz2YEyf7j2wCfWuN3m21QfKehzAwkwBc8boXGYSOJWFm6PAif4iHQ3kbT5xZ5safDeBlt03YDgqr5EhooYR' #os.environ.get("CLIENT_SECRET")
-#INTERVALS_SECRET = 'test'
+
 
 
 SPORT_ORG_ENDPOINT = f"/api/registration/organization/"
@@ -18,5 +18,5 @@ PROFILE_ENDPOINT = f"/api/registration/profile/"
 RAW_INGEST_ENDPOINT = f"/api/warehouse/ingestion/primary/"
 
 # UUID-only: pass the DataSource UUID string directly
-VO2_STEP_SOURCE_UUID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-#WELLNESS_SOURCE_UUID = os.environ.get("WELLNESS_SOURCE_UUID")
+#VO2_STEP_SOURCE_UUID = "os.environ.get("WELLNESS_SOURCE_UUID")
+
